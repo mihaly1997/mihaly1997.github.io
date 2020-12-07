d9a838bb03a36721fb000ccb845cbeeee5b9151d
mihaly1997 <kadar.mihaly2015@gmail.com>
2020-12-07 21:06:16 +0200

    ja 6

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
-var activePage = "home"
+let activePage = "home"
 
 function hide (id){
-    var el = document.getElementById(id);
+    const el = document.getElementById(id);
     console.info
     if (el) {
         el.style.display = "none"
@@ -10,7 +10,7 @@ function hide (id){
 
 function hidePreviousPage(){
     hide (activePage)
-    var link = document.querySelector(`#top-menu-bar a[data-page="${activePage}"]`)
+    const link = document.querySelector(`#top-menu-bar a[data-page="${activePage}"]`)
     link.classList.remove("active")
 }
 
@@ -19,16 +19,16 @@ function hidePreviousPage(){
 function showPage(pageId) {
     hidePreviousPage();
     document.getElementById(pageId).style.display = "";
-    var link = document.querySelector(`#top-menu-bar a[data-page="${pageId}"]`);
+    const link = document.querySelector(`#top-menu-bar a[data-page="${pageId}"]`);
     link.classList.add("active");
     activePage = pageId;
 }
 
 function initMenu() {
     document.addEventListener("click", function(e){
-        var link = e.target;
+        const link = e.target;
         if (link.matches("#top-menu-bar a")) {
-            var id = link.getAttribute("data-page");
+            const id = link.getAttribute("data-page");
             showPage(id);
         }
     })
@@ -39,18 +39,17 @@ initMenu();
 showPage(activePage);
 
 function getHTMLSkills(skills) {
-    var skillsLi = skills.map(function(skill){
+    
+    return skills.map(function(skill){
         return `<li class="${skill.endorsements > 9 ? "favorite" : ""}">
             ${skill.name} <span>&middot; ${skill.endorsements}</span>
         </li>`;
-    });
-    return skillsLi.join("");
+    }).join("");
 }
 
 function showSkills(skills){
-   var html = getHTMLSkills(skills);
-    var ul = document.querySelector("#skills ul");
-    ul.innerHTML = html;
+    const ul = document.querySelector("#skills ul");
+    ul.innerHTML = getHTMLSkills(skills);
 }
  
 fetch("date/skills.json")
@@ -58,6 +57,10 @@ fetch("date/skills.json")
     return r.json();
 })
 .then(function(skills){
+    skills.sort(function(s1, s2) {
+        return s1.endorsements > s2.endorsements ? -1 : 1;
+    });
+
     showSkills(skills);
 });
 
